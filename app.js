0b49bfa5e50f139c2034a6aee78a584b80c55ae0
ZooM982 <134058804+ZooM982@users.noreply.github.com>
2023-08-07 13:56:30 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var mois='janvier', février, mars, avril, mai, juin;
+document.write("<table>");
+document.write("<tr>");
+document.write("<td>"+mois+"</td>");
+document.write("</tr>");
+document.write("</table>");
